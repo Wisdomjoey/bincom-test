d555bbfb27e614eb159408ee20ffd79d68738eb8
Jay Z <85133609+Wisdomjoey@users.noreply.github.com>
2024-07-22 09:02:41 +0000

    minor ui changes

--- components/widgets/goBack.tsx
+++ components/widgets/goBack.tsx
@@ -5,9 +5,9 @@ function GoBack({ url }: { url?: string }) {
   return (
     <Link
       href={url ?? "/"}
-      className="rounded-[50%] border border-textcolor p-3"
+      className="rounded-[50%] border border-textcolor/50 p-1"
     >
-      <BackIcon className="w-5 h-5 stroke-primary" />
+      <BackIcon className="w-6 h-6 stroke-primary" />
     </Link>
   );
 }
